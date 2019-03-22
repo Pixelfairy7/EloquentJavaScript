5785a7529c40d6bec257a3f5012c018ed8302480
Erika <33796496+Pixelfairy7@users.noreply.github.com>
2019-03-22 11:11:04 -0400

    Update Chapter3.js
    Updated code to be more concise and added comments

--- Chapter3.js
+++ Chapter3.js
@@ -1,16 +1,16 @@
 //Minimum 
-//Write a function min that takes 2 arguments and returns their minimum 
+//Write a function min that takes 2 arguments and //returns their minimum 
 
 function min(a,b){
   if(a < b){
     console.log(`${a} is smaller than ${b}`)
-  } else if (a == b){
-    console.log('The values are equal')
-  } else {
+  } else if (b < a ){
     console.log(`${b} is smaller than ${a}`)
+  } else {
+    console.log('The values are equal')
   }
 }
 
-min(2,10);
-min(54746,8);
-min(3,3);
+min(2,10);//2 is smaller than 10
+min(54746,8); //8 is smaller than 54746
+min(3,3); //The values are equal
